221c425fa2be5150187cd83147e8d09631d6a838
majlv <91908439+majlv@users.noreply.github.com>
2021-10-22 10:03:06 -0500

    Additional nested data coming from QB
    Working a solution around this additional nesting

--- importData.js
+++ importData.js
@@ -4,9 +4,6 @@ function importData(context) {
     var apiEndpoint = dataSource.getSetting("apiEndpoint").getValue();
     var startDateString = dataSource.getSetting("Period Range").getValue().getFromPeriodStartDateTime().substring(0, 10);
     var endDateString = dataSource.getSetting("Period Range").getValue().getToPeriodEndDateTime().substring(0, 10);
-
-    var tableId = context.getRowset(['id']).getTableId();
-    var accountList = [];
     
     // Get the Account List from Quickbooks
     var getAccountListMethod = 'GET';
@@ -19,91 +16,73 @@ function importData(context) {
         responseBody = accountListResponse.getBody();
         var accountsData = JSON.parse(responseBody).QueryResponse.Account;
 
-        for (i = 0; i < accountsData.length; i++) {
-            accountList.push({
-                id: accountsData[i].Id,
-                AcctNum: accountsData[i].AcctNum,
-                Name: accountsData[i].Name
-            });
+        ai.log.logInfo("Accounts Data", JSON.stringify(accountsData));
+        ai.log.logInfo(accountsData.length);
+
+        var tableId = context.getRowset(['id']).getTableId();
+        var rowset = ["id", "tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"];
+        var dataRowset = context.getRowset(rowset);
+        
+        // Import the data
+        for (i = 0; i < 5; i++) {
+            var internalActId = accountsData[i].Id;
+            var importDataMethod = 'GET';
+            var importDataBody = '';
+            var importDataHeaders = {};
+            var importDataRequestURL = `${apiEndpoint}/v3/company/${realmID}/reports/GeneralLedger?start_date=${startDateString}&end_date=${endDateString}&accounting_method=Accrual&account=${parseInt(internalActId)}&minorversion=3`;
+            var importDataResponse = ai.https.authorizedRequest(importDataRequestURL, importDataMethod, importDataBody, importDataHeaders);
+            // ai.log.logInfo("Request URL", importDataRequestURL);
+    
+            if (tableId == "adaptive_sum_txns") {
+                try {
+                    // ai.log.logInfo("Trying to get data from source...", `Connecting to ${apiEndpoint}`);
+                    // ai.log.logInfo("Import Data Response HTTP Code", importDataResponse.getHttpCode());
+                    importDataResponse;
+                }
+                catch (exception) {
+                    ai.log.logError('HTTPS connection request failed', +exception);
+                    throw "Connection Error";
+                }
+                
+                if (importDataResponse.getHttpCode() == '200') {
+                    // ai.log.logVerbose('Connection successful. Retrieving data...');
+                    var importDataResponseBody = importDataResponse.getBody();
+                    ai.log.logInfo("Response Body", importDataResponseBody);
+                    
+                    // Locate the embedded object within the JSON response containing the rows of data
+                    var data = JSON.parse(importDataResponseBody).Rows.Row[0].Rows.Row;
+                    var dataHeaderActId = JSON.parse(importDataResponseBody).Rows.Row[0].Header.ColData[0].id;
+                    // ai.log.logInfo("Import Data String Response", JSON.stringify(data));
+                    ai.log.logInfo('Acct ID: ', JSON.stringify(dataHeaderActId));
+                    
+                    ai.log.logInfo('Getting row count...', `${data.length} rows`);
+                    
+                    for (j = 0; j < data.length; j++) {
+                        dataRowset.addRow(
+                            [
+                                dataHeaderActId,                    // Internal QB Account ID
+                                new Date(data[j].ColData[0].value), // Txn Date
+                                Number(data[j].ColData[6].value),   // Txn Amount
+                                Number(data[j].ColData[7].value),   // Account Balance
+                                data[j].ColData[1].value,           // Txn Type
+                                data[j].ColData[2].value,           // Txn doc_num
+                                data[j].ColData[3].value,           // Name
+                                data[j].ColData[4].value,           // Txn Memo
+                                data[j].ColData[5].value            // Txn Split
+                            ]
+                        );
+                    }
+                } else {
+                    ai.log.logError('Error retrieving account data from source.');
+                    throw "Error";
+                }
+            } else {
+                ai.log.logError('tableId does not match current table');
+                throw "Error";
+            }
         }
-        ai.log.logInfo('List of Accounts', JSON.stringify(accountList));
     } else {
         ai.log.logError('Error retreiving list of accounts from source');
         throw "Error";
     }
-        
-    // Iterate through list of accounts and import data into the table
-    // ai.log.logInfo(`Loading table ${tableId}`);
-    // var table = 'adaptive_sum_txns';
-    // var rowset = ["tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"];
-    // var dataRowset = context.getRowset(rowset);
-    // for (i = 0; i < 26; i++) {
-    //         ai.log.logInfo(`starting here with ${accountList[0].id}, ${accountList[0].Name}`);
-    //         importData(table, accountList[i].id, dataRowset);
-    // }
-    
-    // Import Data Function
-    function importData(table, acctId, dataRowset) {
-        
-    ai.log.logInfo(`Loading table ${tableId}`);
-    var table = 'adaptive_sum_txns';
-    var rowset = ["tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"];
-    var dataRowset = context.getRowset(rowset);
-    
-        for (i = 0; i < 26; i++) {
-            ai.log.logInfo(`starting here with ${accountList[0].id}, ${accountList[0].Name}`);
-            var importDataMethod = 'GET';
-            var importDataBody = '';
-            var importDataHeaders = {};
-    
-        if (tableId == table) {
-            // var dataColumns = dataRowset.getColumns();
-            
-            importDataRequestURL = `${apiEndpoint}/v3/company/${realmID}/reports/GeneralLedger?start_date=${startDateString}&end_date=${endDateString}&accounting_method=Accrual&account=${acctId}&minorversion=62`;
-            var importDataResponse = ai.https.authorizedRequest(importDataRequestURL, importDataMethod, importDataBody, importDataHeaders);
-            
-            try {
-                // ai.log.logInfo("Trying to get data from source...", `Connecting to ${apiEndpoint}`);
-                // ai.log.logInfo("import data response", importDataResponse.getHttpCode());
-                importDataResponse;
-            }
-            catch (exception) {
-                ai.log.logError('HTTPS connection request failed', +exception);
-                throw "Connection Error";
-            }
-            
-            if (importDataResponse.getHttpCode() == '200') {
-                ai.log.logVerbose('Connection successful. Retrieving data...');
-                var importDataResponseBody = importDataResponse.getBody();
-                
-                // Locate the embedded object within the JSON response containing the rows of data
-                var data = JSON.parse(importDataResponseBody).Rows.Row[0].Rows.Row;
-                ai.log.logInfo("import data response", JSON.stringify(data));
-                // ai.log.logInfo('detail', data[0].ColdData[0].value);
-                
-                ai.log.logInfo('Getting row count...', `${data.length} rows`);
-                
-                for (i = 0; i < data.length; i++) {
-                    dataRowset.addRow(
-                        [
-                            new Date(data[i].ColData[0].value), // Txn Date
-                            Number(data[i].ColData[6].value),   // Txn amount
-                            Number(data[i].ColData[7].value),   // Account balance
-                            data[i].ColData[1].value,           // Txn Type
-                            data[i].ColData[2].value,           // Txn doc_num
-                            data[i].ColData[3].value,           // Name
-                            data[i].ColData[4].value,           // Txn Memo
-                            data[i].ColData[5].value            // Txn Split
-                        ]
-                    );
-                }
-            } else {
-                ai.log.logError('Error retrieving account data from source.');
-                throw "Error";
-            }
-        } else {
-            ai.log.logError('tableId does not match current table');
-            throw "Error";
-        }
-    } 
-}}
+}
